8da69be09cbc1424aae57b02d898c88452fd119b
vic-dt <victoriavr1323@gmail.com>
2025-03-26 20:38:53 +0100

    Login y rutas hijas

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,25 +5,38 @@ import { createRouter, createWebHashHistory } from "vue-router";
 export const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
+
         {
             path: '/',
-            name: 'home',
-            component: DomusPagina,
+            name: 'landing',
+            component: ()=> import ('@/modulorum/landing/layouts/LandingLayout.vue'),
+            children: [
+                {
+                    path: '/',
+                    name: 'home',
+                    component: DomusPagina,
+                },
+                {
+                    path: '/features',
+                    name: 'features',
+                    component: () => import('@/modulorum/landing/paginae/PeculiaritatesPagina.vue'),
+                },
+                {
+                    path: '/pricing',
+                    name: 'pricing',
+                    component: () => import('@/modulorum/landing/paginae/PretiumPagina.vue'),
+                },
+                {
+                    path: '/contact',
+                    name: 'contact',
+                    component: () => import('@/modulorum/landing/paginae/ContactusPagina.vue'),
+                },
+            ]
         },
+        
         {
-            path: '/features',
-            name: 'features',
-            component: () => import('@/modulorum/landing/paginae/PeculiaritatesPagina.vue'),
-        },
-        {
-            path: '/pricing',
-            name: 'pricing',
-            component: () => import('@/modulorum/landing/paginae/PretiumPagina.vue'),
-        },
-        {
-            path: '/contact',
-            name: 'contact',
-            component: () => import('@/modulorum/landing/paginae/ContactusPagina.vue'),
+            path: '/auth',
+            component: () => import('@/modulorum/auth/paginae/LoginPagina.vue'),
         },
     ]
 });
