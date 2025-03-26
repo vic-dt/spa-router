ea621296e4572650c5acad46d7c2c8fa5056dac4
vic-dt <victoriavr1323@gmail.com>
2025-03-26 20:19:40 +0100

    Páginas del Landing Page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
-    <main class="bg-slate-100">
+    
+    <div class="h-screen flex flex-col"> 
+
+    <header class="flex items-center h-14 sm:h-16 px-4 md:px-6 lg:px-8 border-b border-gray-300">
+        <div>
+            <a href="#">
+                <img src="@/assets/logo.png" width="40" height="40" alt="">
+            </a>
+        </div>
+
         <nav>
-            <RouterLink to="#/">Home</RouterLink>
-            <RouterLink to="#/features">Features</RouterLink>
-            <RouterLink to="#/pricing">Pricing</RouterLink>
-            <RouterLink to="#/contact">Contact</RouterLink>
+            <RouterLink :to="{name: 'home'}">Home</RouterLink>
+            <RouterLink to="/features">Features</RouterLink>
+            <RouterLink to="/pricing">Pricing</RouterLink>
+            <RouterLink to="/contact">Contact</RouterLink>
             
         </nav>
+    </header>
+
+    <main class="flex-1 flex items-center justify-center py-6">
         <RouterView/>
     </main>
+
+    <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
+	<p class="flex-1 text-sm text-gray-500 text-center">
+		© {{ new Date().getFullYear() }} Acme Corporation. Derechos reservados
+	</p>
+</footer>
+
+</div>   
+    
 </template>
 
 <script setup lang="ts">
--- src/modulorum/landing/paginae/DomusPagina.vue
+++ src/modulorum/landing/paginae/DomusPagina.vue
@@ -1,4 +1,11 @@
 
 <template>
-    <h1>Home page</h1>
+    <div class=" text-center ">
+    <h1 class="text-4xl sm:text-5xl font-bold tracking-tighter">
+        Bienvenido a nuestro sitio web
+    </h1>
+    <p class=" md:text-xl text-gray-500 mx-auto w-[600px]" >
+        Donum mox recipies pro fidelitate erga fructus nosotros
+    </p>
+</div>
 </template>
