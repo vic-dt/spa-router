d158d28118e72e4cf3c2527266124ec57456177b
vic-dt <victoriavr1323@gmail.com>
2025-03-28 18:26:45 +0100

    Comenzando la autenticación

--- src/modulorum/auth/paginae/LoginPagina.vue
+++ src/modulorum/auth/paginae/LoginPagina.vue
@@ -36,6 +36,7 @@
   </div>
 
   <button
+  @click="cumLogin"
     type="button"
     class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
   >
@@ -48,3 +49,16 @@
 </div>
 
 </template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const cumLogin = () => {
+  
+  router.replace({
+    name:'home'
+  });
+}
+
+</script>
